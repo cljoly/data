aee15436412ce8dbd96c1e0ab6dd176a85396a43
Clément Joly <l@131719.xyz>
2021-04-10 00:13:14 +0100

    non local script

--- gen_json.sh
+++ gen_json.sh
@@ -3,23 +3,23 @@
 set -e pipe
 set -x
 
-#curl \
-#	-H "Accept: application/vnd.github.mercy-preview+json" \
-#	'https://api.github.com/users/cljoly/repos?page=1&sort=pushed&per_page=100' \
-#	| ~/.local/bin/sqlite-utils insert tmp.db repo -
-#
-#curl \
-#	-H "Accept: application/vnd.github.mercy-preview+json" \
-#	'https://api.github.com/search/issues?q=type:pr+is:merged+author:cljoly&per_page=100' \
-#	| ~/.local/bin/sqlite-utils insert tmp.db repo -
+curl \
+	-H "Accept: application/vnd.github.mercy-preview+json" \
+	'https://api.github.com/users/cljoly/repos?page=1&sort=pushed&per_page=100' \
+	| sqlite-utils insert tmp.db repo -
+
+curl \
+	-H "Accept: application/vnd.github.mercy-preview+json" \
+	'https://api.github.com/search/issues?q=type:pr+is:merged+author:cljoly&per_page=100' \
+	| sqlite-utils insert tmp.db repo -
 
 echo "data retrieved"
 
-~/.local/bin/sqlite-utils create-view tmp.db topic "SELECT DISTINCT \
+sqlite-utils create-view tmp.db topic "SELECT DISTINCT \
 	repo.rowid as rid, json_each.value as t \
 	FROM repo, json_each(topics)"
 
-~/.local/bin/sqlite-utils create-view tmp.db featured_repo "SELECT DISTINCT \
+sqlite-utils create-view tmp.db featured_repo "SELECT DISTINCT \
 	rowid, private, pushed_at, name, html_url, topics, description, stargazers_count \
 	FROM repo WHERE \
 	(fork <> 1 OR rowid IN (SELECT rid FROM topic WHERE t = 'maintained-fork')) \
@@ -28,19 +28,19 @@ echo "data retrieved"
 
 echo "views created"
 
-~/.local/bin/sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
+sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
 	* \
 	FROM featured_repo \
 	WHERE rowid NOT IN (SELECT rid FROM topic WHERE t = 'archived') \
 	ORDER BY pushed_at DESC" > unarchived_repos.json
 
-~/.local/bin/sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
+sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
 	* \
 	FROM featured_repo \
 	WHERE rowid NOT IN (SELECT rid FROM topic WHERE t = 'archived') \
 	ORDER BY pushed_at DESC" > unarchived_repos.json
 
-~/.local/bin/sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
+sqlite-utils query tmp.db --json-cols "SELECT DISTINCT \
 	* \
 	FROM featured_repo \
 	WHERE rowid IN (SELECT rid FROM topic WHERE t = 'archived') \
@@ -48,4 +48,4 @@ echo "views created"
 
 echo "JSONs created"
 
-# rm tmp.db
+rm tmp.db
