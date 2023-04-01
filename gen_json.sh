855f60349780bd6e469a983dd04ac6c1d2c338b8
Clément Joly <foss@131719.xyz>
2023-04-01 01:35:05 +0100

    Misc. fixes

--- gen_json.sh
+++ gen_json.sh
@@ -18,11 +18,13 @@ curl \
 	| yq '[to_entries[] | {lang: .key, color: .value.color}]' \
 	| sqlite-utils insert tmp.db linguist -
 
+sleep 10
 gh api \
 	-H "Accept: application/vnd.github.mercy-preview+json" \
 	'https://api.github.com/users/cljoly/repos?page=1&sort=pushed&per_page=100' \
 	| sqlite-utils insert tmp.db repo -
 
+sleep 10
 gh api \
 	-H "Accept: application/vnd.github.mercy-preview+json" \
 	-q '.items' \
